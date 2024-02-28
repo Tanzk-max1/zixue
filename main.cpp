24415a79f6f3a20c82ed7a60e9a474b273ebe16c
zk_glb <1552535580@qq.com>
2024-02-28 16:49:24 +0800

    first commit-1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+int g_a = 10;
+int g_b = 10;
+
+//全局常量
+const int c_g_a = 10;
+const int c_g_b = 10;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    //局部变量
+    int a = 10;
+    int b = 10;
+    int *d1 = &a;
+    uintptr_t addressValue = reinterpret_cast<uintptr_t>(&a);
+    uintptr_t addressValue1 = reinterpret_cast<uintptr_t>(&b);
+    //打印地址
+    cout << "局部变量a地址为： " << addressValue << endl;
+    cout << "局部变量a地址为： " << addressValue1 << endl;
+
+
     return 0;
+
 }
