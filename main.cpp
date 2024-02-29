bc6c0d54696a361d0f86d4b4b267094de7742a98
zk_glb <1552535580@qq.com>
2024-02-29 18:36:09 +0800

    2.29first commit

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 using namespace std;
-int g_a = 10;
-int g_b = 10;
 
-//全局常量
-const int c_g_a = 10;
-const int c_g_b = 10;
+int* func(){
+    int* a = new int(10);
+    return a;
+}
 
 int main() {
 
-    //局部变量
-    int a = 10;
-    int b = 10;
-    int *d1 = &a;
-    uintptr_t addressValue = reinterpret_cast<uintptr_t>(&a);
-    uintptr_t addressValue1 = reinterpret_cast<uintptr_t>(&b);
-    //打印地址
-    cout << "局部变量a地址为： " << addressValue << endl;
-    cout << "局部变量a地址为： " << addressValue1 << endl;
+    int * arr = new int [10];
+    for (int i = 0; i < 10; ++i) {
+        arr[i] = i + 100;
+
+    }
+    for (int i = 0; i < 10; ++i) {
+        cout << arr[i] << endl;
+
+    }
+    delete[] arr;
+
 
 
     return 0;
