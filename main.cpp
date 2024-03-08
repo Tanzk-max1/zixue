e6ce6cdc2a8bc745c51785bdab81432f35480ef5
zk_glb <1552535580@qq.com>
2024-03-08 23:01:58 +0800

    添加了文件进去

--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int* func(){
-    int* a = new int(10);
-    return a;
-}
+//int& test01(){
+//  int a = 10;
+//  return a; //因为是一个局部变量，局部变量的话在完成引用之后就要被销毁，如果要被合理使用只能是静态变量进行迁移
+//}
 
-void increment(int& num) {
-    num++;
+int& test02(){
+  static int a = 20;
+  return a;
 }
 
 int main() {
 
-    int a = 10;
-    int b = 20;
-    int &c = a;
-    c = b;
+//    int & ref = test01();
+//    cout << "ref = " << ref << endl;
+//    cout << "ref = " << ref << endl;
 
-    cout << "a = " << a << endl;
-    cout << "b = " << b << endl;
-    cout << "c = " << c << endl;
+
+//    cout << "a = " << a << endl;
+//    cout << "b = " << b << endl;
+//    cout << "c = " << c << endl;
+
+    int & ref2 = test02();
+    cout << "ref2 = " << ref2 << endl;
+    cout << "ref2 = " << ref2 << endl;
+
+    test02() = 1000;//此处做了左值
+    cout << "ref21 = " << ref2 << endl;
+    cout << "ref21 = " << ref2 << endl;
+
 
 
     return 0;
