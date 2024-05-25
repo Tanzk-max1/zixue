1710fbbe5bc4ea1ad1fa79622cc8b25e88384dac
zk_glb <1552535580@qq.com>
2024-05-25 22:19:24 +0800

    笔记更新

--- main.cpp
+++ main.cpp
@@ -1970,33 +1970,45 @@ using std::cin;
 //  cout << endl;
 //}
 
-int add(int a, int b) { return a + b; }
-int subtract(int a, int b) { return a - b; }
-int multiply(int a, int b) { return a * b; }
-int divide(int a, int b) { return b != 0 ? a / b : 0; }
+//int add(int a, int b) { return a + b; }
+//int subtract(int a, int b) { return a - b; }
+//int multiply(int a, int b) { return a * b; }
+//int divide(int a, int b) { return b != 0 ? a / b : 0; }
+//
+//int main(){
+//  int func(int a, int b);
+//
+//  using pFunc1 = decltype(func) *;
+//  typedef decltype(func) *pFunc2;
+//  using pFunc3 = int (*)(int a, int b);
+//  using pFunc4 = int(int a, int b);
+//  typedef int(*pFunc5)(int a, int b);
+//  using pFunc6 = decltype(func);
+//
+//
+//
+//  std::vector<pFunc1> vec1;
+//  std::vector<pFunc2> vec2;
+//  std::vector<pFunc3> vec3;
+//  std::vector<pFunc4*> vec4;
+//  std::vector<pFunc5> vec5;
+//  std::vector<pFunc6*> vec6;
+//
+//  std::vector<decltype(func) *> vec{add, subtract, multiply, divide};
+//  for (auto f : vec)
+//    std::cout << f(2, 2) << std::endl;
+//
+//}
+//
+
 
 int main(){
-  int func(int a, int b);
-
-  using pFunc1 = decltype(func) *;
-  typedef decltype(func) *pFunc2;
-  using pFunc3 = int (*)(int a, int b);
-  using pFunc4 = int(int a, int b);
-  typedef int(*pFunc5)(int a, int b);
-  using pFunc6 = decltype(func);
-
-
-
-  std::vector<pFunc1> vec1;
-  std::vector<pFunc2> vec2;
-  std::vector<pFunc3> vec3;
-  std::vector<pFunc4*> vec4;
-  std::vector<pFunc5> vec5;
-  std::vector<pFunc6*> vec6;
-
-  std::vector<decltype(func) *> vec{add, subtract, multiply, divide};
-  for (auto f : vec)
-    std::cout << f(2, 2) << std::endl;
-
+  //写一句代码，在内存0xxxx处写入一个4字节的10
+//  int *&&p =(int*)0x0018ff44;//右值引用。
+  int a=10;
+  int *p = &a;
+  int **q=&p;
+  cout<< typeid(q).name();
+  return 0;
 }
 
